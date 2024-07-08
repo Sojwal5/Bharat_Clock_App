b3ca615c7b8fa1a1f32174dc16d711102d1ce2a2
Sojwal <sojwalkuthe@gmail.com>
2024-07-08 11:01:06 +0530

    initial project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import ClockHeading from './Components/ClockHeading';
+import ClockSlogan from './Components/ClockSlogan';
+import ClockTime from './Components/ClockTIme';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+  return <center>
+    <ClockHeading/>
+    <ClockSlogan/>
+    <ClockTime/>
+  </center>
+    
+  
+}
+
+export default App;
