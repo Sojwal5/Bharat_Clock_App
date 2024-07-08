f079ada17343d88de720a1b946ef37b8c3f82f7c
Sojwal <sojwalkuthe@gmail.com>
2024-07-08 11:05:55 +0530

    changed p

--- src/Components/ClockSlogan.jsx
+++ src/Components/ClockSlogan.jsx
@@ -1,4 +1,4 @@
 let ClockSlogan =() =>{
-    return <p className="lead">This is the clock that shows the time in India</p>
+    return <p className="lead">This is thde clock that shows the time in India</p>
 };
 export default ClockSlogan;
